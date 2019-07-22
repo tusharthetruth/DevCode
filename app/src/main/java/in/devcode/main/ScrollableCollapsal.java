dffe244211fcb4d5a535b795d16a4ae13627248e
tusharnigam88@gmail.com <tusharnigam88@gmail.com>
2019-07-22 23:38:57 +0530

    added parallex

---
+++ app/src/main/java/in/devcode/main/ScrollableCollapsal.java
@@ -0,0 +1,14 @@
+package in.devcode.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import in.devcode.R;
+
+public class ScrollableCollapsal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_collapsal);
+    }
+}
--- app/src/main/java/in/devcode/main/Splash.kt
+++ app/src/main/java/in/devcode/main/Splash.kt
@@ -11,6 +11,6 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this,MainActivity::class.java1))
+        startActivity(Intent(this,ScrollableCollapsal::class.java1))
     }
 }
